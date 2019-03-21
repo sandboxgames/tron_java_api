21b3c5e19ff5f2dfe06f56fd251c225c8ab5cddf
sloanwu <sloanwu@gmail.com>
2019-03-21 16:14:51 +0800

    add getTxid for Transaction

--- src/main/java/org/tron/walletserver/TronVegasUtils.java
+++ src/main/java/org/tron/walletserver/TronVegasUtils.java
@@ -3,6 +3,7 @@ package org.tron.walletserver;
 import org.tron.api.GrpcAPI;
 import org.tron.common.crypto.Sha256Hash;
 import org.tron.common.utils.ByteArray;
+import org.tron.protos.Protocol;
 
 public class TronVegasUtils {
 
@@ -11,6 +12,13 @@ public class TronVegasUtils {
             return "";
         }
         return ByteArray.toHexString(Sha256Hash.hash(transactionExtention.getTransaction().getRawData().toByteArray()));
+    }
+
+    public static String getTxid(Protocol.Transaction transaction){
+        if(transaction == null){
+            return "";
+        }
+        return ByteArray.toHexString(Sha256Hash.hash(transaction.getRawData().toByteArray()));
     }
 
 }
